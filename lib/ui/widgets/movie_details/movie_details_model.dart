1718818e123be43034b71a8232ca744019d400e3
Sergey <Sergey.popov.fly@gmail.com>
2021-08-26 21:43:48 +0300

    model moviedetails

--- lib/ui/widgets/movie_details/movie_details_model.dart
+++ lib/ui/widgets/movie_details/movie_details_model.dart
@@ -1,3 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:intl/intl.dart';
+import 'package:the_movie_db/domain/api_client/api_client.dart';
+import 'package:the_movie_db/domain/entity/movie_details.dart';
 
-class MovieDetailsModel extends ChangeNotifier {}
+class MovieDetailsModel extends ChangeNotifier {
+  final _apiClient = ApiClient();
+
+  final int movieId;
+  MovieDetails? _movieDetails;
+  late final String _locale;
+  late DateFormat _dateFormat;
+
+  MovieDetailsModel({
+    required this.movieId,
+  });
+
+  Future<void> setupLocale(BuildContext context) async {
+    final locale = Localizations.localeOf(context).toLanguageTag();
+    if (locale == _locale) return;
+    _locale = locale;
+    _dateFormat = DateFormat.yMMMEd(locale);
+    await loadDetails();
+  }
+
+  Future<void> loadDetails() async {
+    //пропустил авайт этого надо дождаться
+    _movieDetails = await _apiClient.movieDetails(movieId, _locale);
+    notifyListeners();
+  }
+}
