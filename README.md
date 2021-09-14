2596cbd838d50711cd5e753f88a08dac030d870c
Sergey <Sergey.popov.fly@gmail.com>
2021-09-15 02:51:18 +0300

    readme change

--- README.md
+++ README.md
@@ -26,3 +26,67 @@ State manage:
 For help getting started with Flutter, view our
 [online documentation](https://flutter.dev/docs), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+
+# This is an <h1> tag
+## This is an <h2> tag
+###### This is an <h6> tag
+
+* Item 1
+* Item 2
+ * Item 2a
+ * Item 2b
+ 
+ ![GitHub Logo](/images/logo.png)
+Format: ![Alt Text](url)
+
+*This text will be italic*
+_This will also be italic_
+**This text will be bold**
+__This will also be bold__
+*You **can** combine them*
+
+1. Item 1
+2. Item 2
+3. Item 3
+ * Item 3a
+ * Item 3b
+
+ http://github.com - automatic!
+[GitHub](http://github.com)
+
+As Grace Hopper said:
+> I’ve always been more interested
+> in the future than in the past.
+
+\*literal asterisks\*
+
+#1
+github-flavored-markdown#1
+defunkt/github-flavored-markdown#1
+
+GitHub supports emoji!
+:+1: :sparkles: :camel: :tada:
+:rocket: :metal: :octocat:
+
+```javascript
+function test() {
+ console.log("look ma’, no spaces");
+}
+```
+
+function test() {
+ console.log("look ma’, no spaces");
+}
+
+- [x] this is a complete item
+- [ ] this is an incomplete item
+- [x] @mentions, #refs, [links](),
+**formatting**, and <del>tags</del>
+supported
+- [x] list syntax required (any
+unordered or o
+
+First Header | Second Header
+------------ | -------------
+Content cell 1 | Content cell 2
+Content column 1 | Content column 2
--- lib/ui/widgets/news_screen/news_screen_widget.dart
+++ lib/ui/widgets/news_screen/news_screen_widget.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:palette_generator/palette_generator.dart';
 
 class NewsScreenWidget extends StatefulWidget {
   const NewsScreenWidget({
@@ -10,52 +9,8 @@ class NewsScreenWidget extends StatefulWidget {
 }
 
 class _NewsScreenWidgetState extends State<NewsScreenWidget> {
-  List<Image> images = [];
-  late PaletteGenerator paletColor;
-
-  late List<PaletteGenerator> colors;
-  @override
-  void initState() {
-    super.initState();
-    //  colors = [];
-
-    images.addAll(
-      [
-        Image.network(
-          'https://sun1-19.userapi.com/s/v1/ig1/DQO8ENeBVPGNwCSgSLpaECuCsy781Pidg-Q-attLpaL7k_GwTrUW9pRGowhDcHTkduTJckLD.jpg?size=200x200&quality=96&crop=1,119,954,954&ava=1',
-          filterQuality: FilterQuality.high,
-        ),
-        // Image.network(
-        //   'https://sun1-29.userapi.com/s/v1/if1/R2KrOmcll_adCk3MUo9L60Rp-L8Aa7pWIjkzER13TsrjBpatg1HJtVfyTlV4JUFm-m_ljfdY.jpg?size=200x200&quality=96&crop=107,61,1046,1046&ava=1',
-        //   filterQuality: FilterQuality.high,
-        // ),
-      ],
-    );
-    _updatePalettes();
-  }
-
-  _updatePalettes() async {
-    if (images.isEmpty) return;
-    for (var image in images) {
-      // colors.add(await _generatePalette(image))
-      paletColor = await _generatePalette(image);
-    }
-  }
-
-  // final PaletteGenerator palette;  color: palette.darkMutedColor.color.withOpacity(0.8),
-  Future<PaletteGenerator> _generatePalette(Image image) async {
-    PaletteGenerator _paletteGenerator =
-        await PaletteGenerator.fromImageProvider(image.image);
-    return _paletteGenerator;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: paletColor.dominantColor?.color,
-      child: ListView(
-        children: images,
-      ),
-    );
+    return Container();
   }
 }
